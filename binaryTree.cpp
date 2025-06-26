d9bd4f50e7f3439573d1e1ec8380a1dd8380afdf
wpiskaa <hafizzkurniawan19@gmail.com>
2025-06-26 11:09:09 +0700

    membuat class node

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+class node
+{
+    public:
+    int info;
+    node *leftchild;
+    node *rightchild;
+
+    //Konstruktor class node
+    node()
+    {
+        leftchild = nullptr; // membuat leftchild menjadi NULL
+        rightchild = nullptr; // membuat leftchild menjadi NULL
+    }
+};
